2a878e71a9a5e319f08a4607aa49a96421f819c3
RobinYang11 <756517553@qq.com>
2018-09-09 18:37:16 +0800

    init

---
+++ assets/robin.js
@@ -0,0 +1,18 @@
+
+(function(w,e){
+	console.log(w)
+})(typeof window ? window : global,function(){
+	
+	function r(){
+		
+	}
+
+	r.prototype={
+		et:function(n,o){
+			this.n=o
+		}
+	}
+
+	window.r=r;
+	
+}())
