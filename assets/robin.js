5378a4c96b81d412a6908b776c3cf0163831d798
robinyang11 <756517553@qq.com>
2018-09-14 14:41:06 +0800

    add some function

--- assets/robin.js
+++ assets/robin.js
@@ -1,18 +1,45 @@
 
-(function(w,e){
+(function (w, e) {
 	console.log(w)
-})(typeof window ? window : global,function(){
-	
-	function r(){
-		
+})(typeof window ? window : global, function (w) {
+
+	w.EventTarget.prototype.on=function(e){
+		console.log('ON')
 	}
 
-	r.prototype={
-		et:function(n,o){
-			this.n=o
+
+	w.r || (w.r = {
+		isFunction: function (o) {
+			return Object.propotype.toString.call(o) === "[object Function]";
+		},
+		removeClass: function (d, v) {
+			var clazz = d.className,
+				cs = this.classToArray(clazz),
+				index = cs.indexOf(v),
+				nClazz = '';
+			if (index != -1) {
+				for (i in cs) {
+					if (i != index) {
+						nClazz += cs[i] + " ";
+					}
+				}
+			}
+			d.className = nClazz;
+		},
+		classToArray: function (v) {
+			if (typeof v === "string") {
+				return v.match(/[^\x20\r\n\t\f]+/g) || []
+			};
+			return [];
+		},
+		delegate: function (target, type, fn) {
+			var target = document.querySelectorAll(target)[0];
+			target.addEventListener(type, function (e) {
+				if (e.target.nodeName.toLowerCase() == "li") {
+					fn(e);
+				}
+			})
 		}
-	}
+	})
 
-	window.r=r;
-	
-}())
+}(window))
