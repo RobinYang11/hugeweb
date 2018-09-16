0aa1c639d72f99cb4a8e5556d3fef1b9950a4a0e
robinyang11 <756517553@qq.com>
2018-09-17 03:06:30 +0800

    fsf  dsfe enter the commit message for your changes. Lines starting

--- assets/robin.js
+++ assets/robin.js
@@ -3,29 +3,49 @@
 	console.log(w)
 })(typeof window ? window : global, function (w) {
 
-	w.EventTarget.prototype.on=function(e){
+	w.EventTarget.prototype.on = function (e) {
 		console.log('ON')
 	}
-
 
 	w.r || (w.r = {
 		isFunction: function (o) {
 			return Object.propotype.toString.call(o) === "[object Function]";
 		},
-		removeClass: function (d, v) {
-			var clazz = d.className,
-				cs = this.classToArray(clazz),
-				index = cs.indexOf(v),
-				nClazz = '';
-			if (index != -1) {
-				for (i in cs) {
-					if (i != index) {
-						nClazz += cs[i] + " ";
-					}
-				}
-			}
-			d.className = nClazz;
+		hasClass: function (d, c) {
+			var res = d.className.match(new RegExp(c));
+			return res;
 		},
+		removeClass: function (d, c) {
+			if (this.hasClass(d, c)) {
+				d.className = d.className.replace(c, " ");
+			}
+		},
+		addClass: function (d, c) {
+			if (!this.hasClass(d, c) || d.className != "") {
+				d.className += " " + c;
+			}
+		},
+		toggleClass(d, c) {
+			if (this.hasClass(d, c)) {
+				this.addClass(d, c);
+			} else {
+				this.removeClass(d, c);
+			}
+		},
+		// removeClass: function (d, v) {
+		// 	var clazz = d.className,
+		// 		cs = this.classToArray(clazz),
+		// 		index = cs.indexOf(v),
+		// 		nClazz = '';
+		// 	if (index != -1) {
+		// 		for (i in cs) {
+		// 			if (i != index) {
+		// 				nClazz += cs[i] + " ";
+		// 			}
+		// 		}
+		// 	}
+		// 	d.className = nClazz;
+		// },
 		classToArray: function (v) {
 			if (typeof v === "string") {
 				return v.match(/[^\x20\r\n\t\f]+/g) || []
